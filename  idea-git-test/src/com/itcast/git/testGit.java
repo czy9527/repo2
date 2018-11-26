95a5a949977d69f8316de3d3a111ae7bcc7dfd6b
chenzy <aefasffe@163.com>
2018-11-26 20:39:36 +0800

    第一次

---
+++  idea-git-test/src/com/itcast/git/testGit.java
@@ -0,0 +1,14 @@
+package com.itcast.git;
+
+public class testGit {
+    public static void main(String[] args) {
+        System.out.println("adfasdfadfasdfaef");
+        System.out.println("adfasdfadfasdfaef");
+        System.out.println("adfasdfadfasdfaef");
+        System.out.println("adfasdfadfasdfaef");
+        System.out.println("adfasdfadfasdfaef");
+        System.out.println("adfasdfadfasdfaef");
+        System.out.println("adfasdfadfasdfaef");
+    }
+}
+
